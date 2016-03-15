c323b8f25e10630e1b2243a4028a7e2ba411e9cc
Konstantin Usplenev <usplenievk@undev.ru>
2016-03-15 18:05:20 +0300

    QR code element title removed

--- index.js
+++ index.js
@@ -581,7 +581,6 @@ QRCode.prototype.makeCode = function (sText) {
 	this._oQRCode = new QRCodeModel(_getTypeNumber(sText, this._htOption.correctLevel), this._htOption.correctLevel);
 	this._oQRCode.addData(sText);
 	this._oQRCode.make();
-	this._el.title = sText;
 	this._oDrawing.draw(this._oQRCode);
 	this.makeImage();
 };
